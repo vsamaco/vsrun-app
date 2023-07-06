9f2053607bef00395f3f0779ae01bdb422a8d07b
Vincent Samaco <vsamaco@gmail.com>
2023-07-06 15:43:35 -0700

    create shoe component

---
+++ src/components/Shoes.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { type ShoeProps } from "~/types";
+import { metersToMiles } from "~/utils/activity";
+
+type Props = {
+  shoes: ShoeProps[];
+};
+
+export default function Shoes({ shoes }: Props) {
+  return (
+    <div className="flex flex-col justify-center border-t-4 border-green-300 px-5 py-10 sm:px-10 sm:py-20">
+      <h3 className="flex flex-col justify-center text-8xl font-light text-green-300">
+        SHOES
+      </h3>
+
+      <ul className=" mt-20 space-y-4">
+        {shoes.map((shoe) => (
+          <li key={shoe.id} className="flex">
+            <div className="flex-grow border-l-4 border-green-300 bg-white py-4 pl-4">
+              <div className="text-2xl font-light uppercase sm:text-4xl">
+                {shoe.brand_name}
+              </div>
+              <div className="text-lg sm:text-2xl ">{shoe.model_name}</div>
+            </div>
+            <div className="flex flex-none flex-col justify-center bg-white pr-5 text-2xl font-light sm:text-5xl">
+              {metersToMiles(shoe.distance)} mi
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Run from "~/components/Run";
+import Shoes from "~/components/Shoes";
+import Week from "~/components/Week";
 import { type ActivityProps } from "~/types";
 import { api } from "~/utils/api";
 
@@ -12,6 +14,34 @@ export default function Home() {
   }
 
   const highlightRun = data?.highlightRun as ActivityProps;
+  const weekStats = {
+    start_date: new Date(2023, 5, 29),
+    end_date: new Date(2023, 6, 4),
+    total_runs: 5,
+    total_distance: 12000,
+    total_duration: 30000,
+    total_elevation: 1000,
+  };
+  const shoes = [
+    {
+      id: "123",
+      brand_name: "Saucony",
+      model_name: "Ride 15",
+      distance: 100000,
+    },
+    {
+      id: "234",
+      brand_name: "Adidas",
+      model_name: "Adizero Pro 3",
+      distance: 20000,
+    },
+    {
+      id: "345",
+      brand_name: "New Balance",
+      model_name: "Rebel v3",
+      distance: 10000,
+    },
+  ];
 
   return (
     <>
@@ -22,8 +52,14 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <h1 className="text-white">{data?.username}</h1>
-        <div className="bg-white">
+        <div className="bg-gray-100">
           {data?.highlightRun && <Run activity={highlightRun} />}
+        </div>
+        <div className="bg-gray-100">
+          {data?.highlightRun && <Week weekStats={weekStats} />}
+        </div>
+        <div className="bg-gray-100">
+          {data?.shoes && <Shoes shoes={shoes} />}
         </div>
       </main>
     </>
