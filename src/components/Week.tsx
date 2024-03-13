f9edfa3ea256d3d76f435fa8d095bf1f8c2d6b80
Vincent Samaco <vsamaco@gmail.com>
2024-03-13 15:24:05 -0700

    create graph for week stats

--- src/components/Week.tsx
+++ src/components/Week.tsx
@@ -1,12 +1,11 @@
-import { format } from "date-fns";
+import { ResponsiveBar } from "@nivo/bar";
+import { format, getDay } from "date-fns";
 import React from "react";
-import { type WeekStat } from "~/types";
+import { type Activity, type WeekStat } from "~/types";
 import {
   formatHumanizeSeconds,
   metersToFeet,
   metersToMiles,
-  parseDateDay,
-  parseDateMonth,
 } from "~/utils/activity";
 import { formatDate } from "~/utils/date";
 
@@ -71,8 +70,143 @@ function Week({ weekStats }: WeekProps) {
           </div>
         </li>
       </ul>
+      {weekStats?.activities.length > 0 && (
+        <MyResponsiveBar activities={weekStats.activities} />
+      )}
     </div>
   );
 }
+
+const MyResponsiveBar = ({ activities }: { activities: Activity[] }) => {
+  const weekData = [
+    { id: "monday", value: 0 },
+    { id: "tuesday", value: 0 },
+    { id: "wednesday", value: 0 },
+    { id: "thursday", value: 0 },
+    { id: "friday", value: 0 },
+    { id: "saturday", value: 0 },
+    { id: "sunday", value: 0 },
+  ];
+
+  activities.forEach((activity) => {
+    let weekIndex = getDay(new Date(activity.start_date)) - 1;
+    if (weekIndex === -1) weekIndex = 6;
+
+    console.log(weekIndex, activity.start_date, activity.name);
+    if (weekIndex in weekData) {
+      weekData[weekIndex]!.value += metersToMiles(activity.distance);
+    }
+  });
+
+  console.log(weekData);
+
+  return (
+    <div style={{ height: 400 }}>
+      <ResponsiveBar
+        data={weekData}
+        keys={["value"]}
+        indexBy="id"
+        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+        padding={0.3}
+        valueScale={{ type: "linear" }}
+        indexScale={{ type: "band", round: true }}
+        colors={{ scheme: "nivo" }}
+        defs={[
+          {
+            id: "dots",
+            type: "patternDots",
+            background: "inherit",
+            color: "#38bcb2",
+            size: 4,
+            padding: 1,
+            stagger: true,
+          },
+          {
+            id: "lines",
+            type: "patternLines",
+            background: "inherit",
+            color: "#eed312",
+            rotation: -45,
+            lineWidth: 6,
+            spacing: 10,
+          },
+        ]}
+        fill={[
+          {
+            match: {
+              id: "fries",
+            },
+            id: "dots",
+          },
+          {
+            match: {
+              id: "sandwich",
+            },
+            id: "lines",
+          },
+        ]}
+        borderColor={{
+          from: "color",
+          modifiers: [["darker", 1.6]],
+        }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "day of week",
+          legendPosition: "middle",
+          legendOffset: 32,
+          // truncateTickAt: 0,
+        }}
+        axisLeft={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "distance (mi)",
+          legendPosition: "middle",
+          legendOffset: -40,
+          // truncateTickAt: 0,
+        }}
+        labelSkipWidth={12}
+        labelSkipHeight={12}
+        labelTextColor={{
+          from: "color",
+          modifiers: [["darker", 1.6]],
+        }}
+        // legends={[
+        //   {
+        //     dataFrom: "keys",
+        //     anchor: "bottom-right",
+        //     direction: "column",
+        //     justify: false,
+        //     translateX: 120,
+        //     translateY: 0,
+        //     itemsSpacing: 2,
+        //     itemWidth: 100,
+        //     itemHeight: 20,
+        //     itemDirection: "left-to-right",
+        //     itemOpacity: 0.85,
+        //     symbolSize: 20,
+        //     effects: [
+        //       {
+        //         on: "hover",
+        //         style: {
+        //           itemOpacity: 1,
+        //         },
+        //       },
+        //     ],
+        //   },
+        // ]}
+        role="application"
+        ariaLabel="Nivo bar chart demo"
+        barAriaLabel={(e) =>
+          `${e.id} : ${e.formattedValue} in country: ${e.indexValue}`
+        }
+      />
+    </div>
+  );
+};
 
 export default Week;
--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -96,6 +96,7 @@ export const WeekSettingsFormSchema = z.object({
     .min(1),
   start_date: z.coerce.date(),
   end_date: z.coerce.date(),
+  activities: z.array(RunSettingsFormSchema),
 });
 
 export const ShoeSettingsFormSchema = z.object({
