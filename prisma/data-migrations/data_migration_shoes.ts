33cda46cedf153edc49e3297fc3784e702942a99
Vincent Samaco <vsamaco@gmail.com>
2024-04-07 00:30:30 -0700

    setup shoe migration args and update tx timeout

--- prisma/data-migrations/data_migration_shoes.ts
+++ prisma/data-migrations/data_migration_shoes.ts
@@ -1,82 +1,110 @@
 import { PrismaClient } from "@prisma/client";
+import yargs from "yargs/yargs";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const writeShoes = process.argv.indexOf("-f") !== -1;
-  console.log(writeShoes ? "MIGRATE SHOES WRITE" : "MIGRATE SHOES READ");
+  const argv = yargs(process.argv.slice(2))
+    .options({
+      f: { type: "boolean", default: false },
+      profile: { type: "array" },
+      allProfiles: { type: "boolean", default: false },
+    })
+    .parseSync();
 
-  await prisma.$transaction(async (tx) => {
-    const profiles = await tx.runProfile.findMany({
-      include: {
-        shoeRotations: {
-          include: { shoeList: true },
-        },
-        shoes2: true,
-      },
-    });
+  const writeShoes = argv.f;
+  console.log("ARGS", {
+    f: argv.f,
+    profile: argv.profile,
+    allProfiles: argv.allProfiles,
+  });
 
-    for (const profile of profiles) {
-      console.log("\nprocess profile:", profile.name);
-      const { shoeRotations, shoes2: shoeList } = profile;
+  console.log(writeShoes ? `MIGRATE SHOES WRITE` : `MIGRATE SHOES READ`);
 
-      for (const shoeRotation of shoeRotations) {
-        console.log("\nprocess shoeRotation:" + shoeRotation.name);
-        const currentShoes = shoeRotation.shoes;
-
-        for (const shoe of currentShoes) {
-          const foundShoe = shoeList.find(
-            (s) =>
-              s.brand_name === shoe.brand_name &&
-              s.model_name === shoe.model_name &&
-              s.distance === shoe.distance
-          );
-
-          if (!foundShoe) {
-            // migrate existing shoe
-            console.log(`create shoe: ${shoe.brand_name} ${shoe.model_name}`);
-            if (writeShoes) {
-              const shoeResult = await tx.shoe.create({
-                data: {
-                  ...shoe,
-                  start_date: new Date(),
-                  description: shoe.description || "",
-                  runProfileId: profile.id,
-                  shoeRotations: {
-                    connect: { id: shoeRotation.id },
-                  },
-                },
-              });
-              console.log(`shoe id: ${shoeResult.id}`);
-              shoeList.push(shoeResult);
+  await prisma.$transaction(
+    async (tx) => {
+      const profiles = await tx.runProfile.findMany({
+        ...(argv.profile?.length
+          ? {
+              where: {
+                slug: { in: argv.profile as string[] },
+              },
             }
-          } else {
-            // check shoeRotation relation
-            const needShoeRotation =
-              foundShoe &&
-              !shoeRotation.shoeList.find((s) => s.id === foundShoe.id);
+          : null),
+        include: {
+          shoeRotations: {
+            include: { shoeList: true },
+          },
+          shoes2: true,
+        },
+      });
 
-            if (needShoeRotation) {
-              await tx.shoe.update({
-                where: { id: foundShoe.id },
-                data: {
-                  shoeRotations: {
-                    connect: { id: shoeRotation.id },
+      for (const profile of profiles) {
+        console.log("\nprocess profile:", profile.slug);
+        const { shoeRotations, shoes2: shoeList } = profile;
+
+        for (const shoeRotation of shoeRotations) {
+          console.log("\nprocess shoeRotation:" + shoeRotation.name);
+          const currentShoes = shoeRotation.shoes;
+
+          for (const shoe of currentShoes) {
+            const foundShoe = shoeList.find(
+              (s) =>
+                s.brand_name === shoe.brand_name &&
+                s.model_name === shoe.model_name &&
+                s.distance === shoe.distance
+            );
+
+            if (!foundShoe) {
+              // migrate existing shoe
+              console.log(`create shoe: ${shoe.brand_name} ${shoe.model_name}`);
+              if (writeShoes) {
+                const shoeResult = await tx.shoe.create({
+                  data: {
+                    ...shoe,
+                    start_date: new Date(),
+                    description: shoe.description || "",
+                    runProfileId: profile.id,
+                    shoeRotations: {
+                      connect: { id: shoeRotation.id },
+                    },
                   },
-                },
-              });
-              console.log(
-                "update shoeRotation connection:" +
-                  shoe.brand_name +
-                  " " +
-                  shoe.model_name
-              );
+                });
+                console.log(`shoe id: ${shoeResult.id}`);
+                shoeList.push(shoeResult);
+              }
+            } else {
+              // check shoeRotation relation
+              const needShoeRotation =
+                foundShoe &&
+                !shoeRotation.shoeList.find((s) => s.id === foundShoe.id);
+
+              if (needShoeRotation) {
+                await tx.shoe.update({
+                  where: { id: foundShoe.id },
+                  data: {
+                    shoeRotations: {
+                      connect: { id: shoeRotation.id },
+                    },
+                  },
+                });
+                console.log(
+                  "update shoeRotation connection:" +
+                    shoe.brand_name +
+                    " " +
+                    shoe.model_name
+                );
+              }
             }
           }
         }
       }
+    },
+    {
+      maxWait: 5000, // default: 2000
+      timeout: 20000, // default: 5000
     }
-  });
+  );
 }
 
 main()
