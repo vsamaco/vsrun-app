bf8a3dda3dd67f4c91702849d2a6a40ed9a3ec36
Vincent Samaco <vsamaco@gmail.com>
2024-04-10 15:13:48 -0700

    use recharts for week distance bar

--- src/components/Week.tsx
+++ src/components/Week.tsx
@@ -1,4 +1,3 @@
-import { ResponsiveBar } from "@nivo/bar";
 import { format, getDay } from "date-fns";
 import React from "react";
 import { type Activity, type WeekStat } from "~/types";
@@ -8,6 +7,17 @@ import {
   metersToMiles,
 } from "~/utils/activity";
 import { formatDate } from "~/utils/date";
+
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Calendar, Footprints, Mountain, Timer } from "lucide-react";
 
 type WeekProps = {
   weekStats: WeekStat | Record<string, never>;
@@ -36,176 +46,113 @@ function Week({ weekStats }: WeekProps) {
       <div className="mb-5 border-b-4 border-blue-300">
         <h2 className="text-6xl text-blue-300">WEEK</h2>
       </div>
-
-      <ul className="space-y-5 divide-y divide-gray-500">
-        <li className="flex w-full items-center justify-between pt-5">
-          <div className="text-xl uppercase md:text-2xl">period</div>
-          <div
-            className="hidden text-2xl font-thin uppercase md:text-4xl"
-            ref={periodRef}
-          >
-            {formatDate(new Date(weekStats.start_date), {
-              year: "numeric",
-              month: "2-digit",
-              day: "numeric",
-            })}
-          </div>
-        </li>
-        <li className="flex w-full items-center justify-between pt-5">
-          <div className="text-xl uppercase md:text-2xl">distance</div>
-          <div className="text-2xl font-thin md:text-4xl">
-            {metersToMiles(total_distance).toLocaleString()} mi
-          </div>
-        </li>
-        <li className="flex w-full items-center justify-between pt-5">
-          <div className="text-xl uppercase md:text-2xl">duration</div>
-          <div className="text-2xl font-thin md:text-4xl">
-            {formatHumanizeSeconds(total_duration)}
-          </div>
-        </li>
-        <li className="flex w-full items-center justify-between pt-5">
-          <div className="text-xl uppercase md:text-2xl">elevation</div>
-          <div className="text-2xl font-thin md:text-4xl">
-            {Math.ceil(metersToFeet(total_elevation))} ft
-          </div>
-        </li>
-      </ul>
-      {weekStats.activities && weekStats.activities.length > 0 && (
-        <MyResponsiveBar activities={weekStats.activities} />
+      <div className="flex flex-row items-center space-x-2">
+        <Calendar className="h-6 w-6 text-muted-foreground" />
+        <div className="hidden text-xl uppercase md:text-2xl" ref={periodRef}>
+          {formatDate(new Date(weekStats.start_date), {
+            year: "numeric",
+            month: "2-digit",
+            day: "numeric",
+          })}
+        </div>
+      </div>
+      <div className="my-5 grid gap-4 md:grid-cols-3">
+        <Card className="">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-md font-normal uppercase md:text-xl">
+              Distance
+            </CardTitle>
+            <Footprints className="h-6 w-6 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-xl font-normal md:text-2xl">
+              {metersToMiles(total_distance).toLocaleString()} mi
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-md font-normal uppercase md:text-xl">
+              Duration
+            </CardTitle>
+            <Timer className="h-6 w-6 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-xl font-normal md:text-2xl">
+              {formatHumanizeSeconds(total_duration)}
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-md font-normal uppercase md:text-xl">
+              Elevation
+            </CardTitle>
+            <Mountain className="h-6 w-6 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-xl font-normal md:text-2xl">
+              {Math.ceil(metersToFeet(total_elevation))} ft
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+      {weekStats.activities && (
+        <WeekDistanceBar activities={weekStats.activities} />
       )}
     </div>
   );
 }
 
-const MyResponsiveBar = ({ activities }: { activities: Activity[] }) => {
+const WeekDistanceBar = ({ activities }: { activities: Activity[] }) => {
   const weekData = [
-    { id: "monday", value: 0 },
-    { id: "tuesday", value: 0 },
-    { id: "wednesday", value: 0 },
-    { id: "thursday", value: 0 },
-    { id: "friday", value: 0 },
-    { id: "saturday", value: 0 },
-    { id: "sunday", value: 0 },
+    { name: "Mon", total: 0 },
+    { name: "Tue", total: 0 },
+    { name: "Wed", total: 0 },
+    { name: "Thu", total: 0 },
+    { name: "Fri", total: 0 },
+    { name: "Sat", total: 0 },
+    { name: "Sun", total: 0 },
   ];
 
   activities.forEach((activity) => {
     let weekIndex = getDay(new Date(activity.start_date)) - 1;
     if (weekIndex === -1) weekIndex = 6;
 
-    console.log(weekIndex, activity.start_date, activity.name);
+    // console.log(weekIndex, activity.start_date, activity.name);
     if (weekIndex in weekData) {
-      weekData[weekIndex]!.value += metersToMiles(activity.distance);
+      weekData[weekIndex]!.total += metersToMiles(activity.distance);
     }
   });
 
-  console.log(weekData);
+  // console.log(weekData);
 
   return (
-    <div style={{ height: 400 }}>
-      <ResponsiveBar
-        data={weekData}
-        keys={["value"]}
-        indexBy="id"
-        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-        padding={0.3}
-        valueScale={{ type: "linear" }}
-        indexScale={{ type: "band", round: true }}
-        colors={{ scheme: "nivo" }}
-        defs={[
-          {
-            id: "dots",
-            type: "patternDots",
-            background: "inherit",
-            color: "#38bcb2",
-            size: 4,
-            padding: 1,
-            stagger: true,
-          },
-          {
-            id: "lines",
-            type: "patternLines",
-            background: "inherit",
-            color: "#eed312",
-            rotation: -45,
-            lineWidth: 6,
-            spacing: 10,
-          },
-        ]}
-        fill={[
-          {
-            match: {
-              id: "fries",
-            },
-            id: "dots",
-          },
-          {
-            match: {
-              id: "sandwich",
-            },
-            id: "lines",
-          },
-        ]}
-        borderColor={{
-          from: "color",
-          modifiers: [["darker", 1.6]],
-        }}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "day of week",
-          legendPosition: "middle",
-          legendOffset: 32,
-          // truncateTickAt: 0,
-        }}
-        axisLeft={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "distance (mi)",
-          legendPosition: "middle",
-          legendOffset: -40,
-          // truncateTickAt: 0,
-        }}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
-        labelTextColor={{
-          from: "color",
-          modifiers: [["darker", 1.6]],
-        }}
-        // legends={[
-        //   {
-        //     dataFrom: "keys",
-        //     anchor: "bottom-right",
-        //     direction: "column",
-        //     justify: false,
-        //     translateX: 120,
-        //     translateY: 0,
-        //     itemsSpacing: 2,
-        //     itemWidth: 100,
-        //     itemHeight: 20,
-        //     itemDirection: "left-to-right",
-        //     itemOpacity: 0.85,
-        //     symbolSize: 20,
-        //     effects: [
-        //       {
-        //         on: "hover",
-        //         style: {
-        //           itemOpacity: 1,
-        //         },
-        //       },
-        //     ],
-        //   },
-        // ]}
-        role="application"
-        ariaLabel="Nivo bar chart demo"
-        barAriaLabel={(e) =>
-          `${e.id} : ${e.formattedValue} in country: ${e.indexValue}`
-        }
-      />
-    </div>
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart data={weekData} margin={{ left: -20 }}>
+        <XAxis
+          dataKey="name"
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+        />
+        <YAxis
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+          tickFormatter={(value) => `${value as string}`}
+        />
+        <Tooltip />
+        <Bar
+          dataKey="total"
+          fill="currentColor"
+          radius={[4, 4, 0, 0]}
+          className="text-blue-300"
+        />
+      </BarChart>
+    </ResponsiveContainer>
   );
 };
 
